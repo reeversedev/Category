b70a865becec3c81d1d703587f71bb98d42298dd
Prateek Gogia <prateek_gogia@gridle.io>
2018-11-07 11:24:25 +0530

    Added Home controller

---
+++ client/js/app.js
@@ -0,0 +1,58 @@
+angular
+  .module("app", ["lbServices", "ui.router"])
+  .config([
+    "$stateProvider",
+    "$urlRouterProvider",
+    function($stateProvider, $urlRouterProvider) {
+      $stateProvider
+        .state("login", {
+          url: "/login",
+          templateUrl: "views/login.html",
+          controller: "LoginController"
+        })
+        .state("home", {
+          url: "/home",
+          templateUrl: "views/home.html",
+          controller: "HomeController"
+        })
+        .state("register", {
+          url: "register",
+          templateUrl: "views/register.html",
+          controller: "RegisterController"
+        });
+
+      $urlRouterProvider.otherwise("login");
+    }
+  ])
+  .run([
+    "$rootScope",
+    "$location",
+    "$http",
+    "User",
+    function($rootScope, $location, $http, User) {
+      console.log(User.isAuthenticated());
+
+      $rootScope.$on("$stateChangeStart", function(
+        event,
+        toState,
+        toParams,
+        fromState,
+        fromParams
+      ) {
+        $("#ui-view").html("");
+        $(".page-loading").removeClass("hidden");
+      });
+      $rootScope.$on("$locationChangeStart", function(event, next, current) {
+        // Redirecting the user to login page if not logged in or trying to access a restricted page.
+        var restrictedPage =
+          $.inArray($location.path(), ["/login", "/register"]) === -1;
+        if (restrictedPage && !User.isAuthenticated()) {
+          console.log("User is not authenticated");
+          $location.path("/login");
+        }
+        if (User.isAuthenticated()) {
+          $location.path("/home");
+        }
+      });
+    }
+  ]);
